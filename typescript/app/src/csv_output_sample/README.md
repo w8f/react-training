6d090085022f8b9e5915716ba21eefecb60a3ba8
w8f <haya22aws@gmail.com>
2022-05-31 22:58:33 +0900

    :construction: csv出力するサンプルプログラムを作成中(WIP)

--- typescript/app/src/csv_output_sample/README.md
+++ typescript/app/src/csv_output_sample/README.md
@@ -3,4 +3,67 @@
 ## 技術(パッケージ選定)
 
 - **csv-writer** \
-  csv 書き込み用ライブラリ
+   csv 書き込み用ライブラリ\
+  <https://www.npmjs.com/package/csv-writer>
+
+- axios\
+  非同期通信ライブラリ\
+  <https://www.npmjs.com/package/axios>
+
+## サンプルプログラムの仕様
+
+### input
+
+ユーザ情報のモックを json で返却してくれる REST API\
+<https://jsonplaceholder.typicode.com/users>
+
+```
+[
+  {
+    id: 1,
+    name: 'Leanne Graham',
+    username: 'Bret',
+    email: 'Sincere@april.biz',
+    address: {
+      street: 'Kulas Light',
+      suite: 'Apt. 556',
+      city: 'Gwenborough',
+      zipcode: '92998-3874',
+      geo: [Object]
+    },
+    phone: '1-770-736-8031 x56442',
+    website: 'hildegard.org',
+    company: {
+      name: 'Romaguera-Crona',
+      catchPhrase: 'Multi-layered client-server neural-net',
+      bs: 'harness real-time e-markets'
+    }
+  },
+  ...
+]
+```
+
+### output
+
+csv_output_sample ディレクトリ内の main.ts 実行後、data ディレクトリに CSV ファイルが出力される。
+
+**【出力対象の csv ファイルの仕様】**
+
+| KEY        | 仕様                         |
+| ---------- | ---------------------------- |
+| 文字コード | UTF-8                        |
+| ヘッダ行   | あり                         |
+| 改行コード | LF                           |
+| 値         | ダブルクォーテーションで囲む |
+
+### トラブルシューティング
+
+- ts ファイルを直接実行
+
+```
+/usr/src/app #　./node_modules/.bin/ts-node src/csv_output_sample/main.ts
+```
+
+- CSV ファイルの取り決めに関するキメ\
+  この記事が参考になりそう。\
+  <https://codezine.jp/article/detail/2364>
--- typescript/app/src/csv_output_sample/main.ts
+++ typescript/app/src/csv_output_sample/main.ts
@@ -1,7 +1,19 @@
 "use strict";
 
+/**
+ * Node.js上でCSV出力するサンプルコード
+ */
+const axios = require("axios");
+const fs = require("fs");
+const path = require("path");
+const createCsvWriter = require("csv-writer").createObjectCsvWriter;
+const outputDir = path.join(__dirname, "data");
+
+/**
+ * 現在日時を取得し、YYYYMMDDhhmm形式の文字列を返却する
+ */
 const now = () => {
-  // serverの日時がJSTで取得できないので、一旦ソースで指定
+  // serverの日時がJSTで取得できないので、タイムゾーンは直接指定
   const dt = new Date(
     new Date().toLocaleString("ja", {
       timeZone: "Asia/Tokyo",
@@ -22,27 +34,71 @@ const now = () => {
     return n < 10 ? "0" + n : String(n);
   };
 
-  return `${YYYY}-${__f(MM)}-${__f(DD)}-${__f(hh)}-${__f(mm)}`;
+  return `${YYYY}${__f(MM)}${__f(DD)}${__f(hh)}${__f(mm)}`;
 };
 
-/**
- * Node.js上でCSV出力するサンプルコード
- */
-const axios = require("axios");
-const fs = require("fs");
-const createCsvWriter = require("csv-writer").createObjectCsvWriter;
+const columns = ["id", "name", "username", "email", "phone", "website"];
 const csvWriter = createCsvWriter({
-  path: `./data/output_${now()}.csv`,
+  path: `${outputDir}/output_${now()}.csv`,
+  header: columns.map((column) => {
+    return {
+      id: column,
+      title: column.toUpperCase(),
+    };
+  }),
+  alwaysQuote: true,
 });
+
+/**
+ * ファイルを削除
+ * @param file file名
+ */
+const removeFile = (file: string) => {
+  fs.unlink(file, (err: Error) => {
+    if (err) {
+      throw err;
+    }
+    console.log("file remove success");
+  });
+};
 
 const main = async () => {
   const res = await axios.get("https://jsonplaceholder.typicode.com/users");
   const users = res.data;
-
-  // console.log(users);
+  try {
+    const isCsv = new RegExp(/.*\.csv$/);
+    fs.readdir(
+      "./src/csv_output_sample/data",
+      {
+        encoding: "utf8",
+      },
+      (err: Error, files: string[]) => {
+        if (err) {
+          throw err;
+        }
+        files
+          .filter((file) => {
+            const filePath = path.join(__dirname, "data", file);
+            return fs.statSync(filePath).isFile() && isCsv.test(filePath);
+          })
+          // 既存ファイル削除
+          .forEach((targetFile) => {
+            removeFile(path.join(__dirname, "data", targetFile));
+          });
+      }
+    );
+    // csv 出力
+    csvWriter
+      .writeRecords(users)
+      .then(() => {
+        console.log("done");
+      })
+      .catch((error: Error) => {
+        console.error(error);
+      });
+  } catch (error) {
+    console.error(error);
+  }
 };
 
-console.log(now());
 main();
-
-const dt = new Date();
